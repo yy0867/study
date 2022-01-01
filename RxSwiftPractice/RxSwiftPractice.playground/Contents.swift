104073a83e8f10716544b5c99159571997de447f
김세영 <yy0867@gmail.com>
2022-01-01 10:30:03 +0900

    Add RxSwiftPractice Playground

--- RxSwiftPractice/RxSwiftPractice.playground/Contents.swift
+++ RxSwiftPractice/RxSwiftPractice.playground/Contents.swift
@@ -1,4 +1,35 @@
-import UIKit
+import Foundation
+import RxSwift
+import RxRelay
+
+struct Plan {
+    var name: String
+    var id: Int
+    var color: String
+    var notification: Bool
+    var achieve: Bool
+}
+
+//let selectedDate: PublishSubject<Date> = PublishSubject()
+//let week: PublishRelay<[Date]> = PublishRelay()
+//let planList: PublishRelay<[Plan]> = PublishRelay()
+//let disposeBag = DisposeBag()
+let calendar = Calendar(identifier: .gregorian)
+
+func getWeekBelongsDate(_ date: Date) {
+    let weekOfYear = calendar.component(.weekOfYear, from: date)
+    let component = DateComponents.init(calendar: calendar, timeZone: TimeZone(abbreviation: "KST"), weekOfYear: weekOfYear)
+    print(component.date)
+}
+
+//func bindWeekPlanListToSelectedDate() {
+//    week.subscribe(selectedDate)
+//}
+//
+//func changeDate(to date: Date) {
+//    selectedDate.onNext(date)
+//}
 
 
-var greeting = "Hello, playground"
+// MARK: - Execution
+getWeekBelongsDate(DateComponents(calendar: calendar, timeZone: TimeZone(abbreviation: "KST"), year: 2022, month: 1, day: 1).date!)
