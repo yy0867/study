55e45c109fb6dc8ca248b4e6b289c9df0fe3bdb7
Seyoung Kim <yy0867@gmail.com>
2022-12-29 14:49:07 +0900

    폴더 구조 정리

--- RxSwiftPractice/Pods/Target Support Files/Pods-RxSwiftPractice-RxSwiftPracticeUITests/Pods-RxSwiftPractice-RxSwiftPracticeUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_RxSwiftPractice_RxSwiftPracticeUITests : NSObject
-@end
-@implementation PodsDummy_Pods_RxSwiftPractice_RxSwiftPracticeUITests
-@end
--- RxSwiftPractice/Pods/Target Support Files/Pods-RxSwiftPracticeTests/Pods-RxSwiftPracticeTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_RxSwiftPracticeTests : NSObject
-@end
-@implementation PodsDummy_Pods_RxSwiftPracticeTests
-@end
